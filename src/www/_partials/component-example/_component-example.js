156b6157754bb536c116043dfdbddf2f32fc4cf7
Nathan Rogan <nathanroagn@centro.org.uk>
2020-10-19 10:48:49 +0100

    fix: highlight js - wmcads

--- src/www/_partials/component-example/_component-example.js
+++ src/www/_partials/component-example/_component-example.js
@@ -4,14 +4,14 @@ function showCode(ele) {
     const btn = e.target || e.srcElement; // Set btn to the element from which the click came
 
     // if the btn clicked contains this class, then it must be active, so reset it back to the norm
-    if (btn.classList.contains('wmnds-js-show-code--active')) {
+    if (btn.classList.contains('wmcads-js-show-code--active')) {
       btn.previousSibling.querySelector('.hljs').style.maxHeight = '200px';
-      btn.classList.remove('wmnds-js-show-code--active');
+      btn.classList.remove('wmcads-js-show-code--active');
       btn.innerText = 'Show more code';
     } else {
       // Else expand the code preview to 100%
       btn.previousSibling.querySelector('.hljs').style.maxHeight = '100%';
-      btn.classList.add('wmnds-js-show-code--active');
+      btn.classList.add('wmcads-js-show-code--active');
       btn.innerText = 'Show less code';
     }
   }
@@ -35,8 +35,8 @@ export { showCode };
 
 const displayShowMore = element => {
   // If the code preview is 192 height (without padding) then we need to display the 'show more code' button
-  if (element.clientHeight >= 192 && !element.classList.contains('wmnds-show-more-ignore')) {
-    const htmlString = '<a href="#" class="wmnds-link wmnds-js-show-code">Show more code</a>';
+  if (element.clientHeight >= 192 && !element.classList.contains('wmcads-show-more-ignore')) {
+    const htmlString = '<a href="#" class="wmcads-link wmcads-js-show-code">Show more code</a>';
 
     element.parentElement.insertAdjacentHTML('afterend', htmlString);
   }
@@ -51,7 +51,7 @@ export default () => {
     hljs.highlightBlock(element);
     displayShowMore(element);
   });
-  [].forEach.call(document.querySelectorAll('.wmnds-js-show-code'), ele => {
+  [].forEach.call(document.querySelectorAll('.wmcads-js-show-code'), ele => {
     showCode(ele); // run show code function above when hljs has init
   });
 };
