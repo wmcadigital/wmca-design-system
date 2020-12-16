f7b63aab3736638cb3afbf2064ec837e5eda25c2
Nathan Rogan <rolorogan@gmail.com>
2020-12-16 11:40:57 +0000

    fix: icon sprite link

--- src/www/wmcads-website.js
+++ src/www/wmcads-website.js
@@ -16,7 +16,7 @@ import {
 const icons = () => {
   // Ajax SVG in, SVGS are referenced in app (Icon component)
   const ajax = new XMLHttpRequest();
-  ajax.open('GET', '$*cdn/img/wmcads-icons.min.svg', true);
+  ajax.open('GET', '/img/wmcads-icons.min.svg', true);
   ajax.send();
   ajax.onload = () => {
     const div = document.createElement('div');
