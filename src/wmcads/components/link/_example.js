5b3ad18c1a725898db0304bfdf14042436354ba7
Nathan Rogan <nathanroagn@centro.org.uk>
2021-03-15 17:21:26 +0000

    new remad more button

---
+++ src/wmcads/components/link/_example.js
@@ -0,0 +1,110 @@
+const linkJS = () => {
+// get all the 'trigger' elements on the page
+const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+
+// listen to click events that occuor
+// only on our triggers
+window.addEventListener(
+  'click',
+  ev => {
+    const elm = ev.target;
+    if (triggers.includes(elm)) {
+      ev.preventDefault();
+      const selector = elm.getAttribute('data-target');
+      collapse(selector, 'toggle');
+      if (elm.getAttribute('aria-expanded') === 'false') {
+        elm.setAttribute('aria-expanded', 'true');
+      } else {
+        elm.setAttribute('aria-expanded', 'false');
+      }
+    }
+  },
+  false
+);
+
+// map commands to the classList methods
+const fnmap = {
+  toggle: 'toggle',
+  show: 'add',
+  hide: 'remove'
+};
+const collapse = (selector, cmd) => {
+  const targets = Array.from(document.querySelectorAll(selector));
+  targets.forEach(target => {
+    target.classList[fnmap[cmd]]('show');
+  });
+};
+
+
+
+const tabs = document.querySelectorAll('ul.wdgt-tab-labels > li');
+const panelQuestions = document.querySelectorAll('.wdgt-panel-question');
+
+const onTabClick = e => {
+  e.preventDefault();
+
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  const clickedTab = e.currentTarget;
+  clickedTab.classList.add('active');
+
+  const tabsPanel = document.querySelectorAll('.wdgt-single-panel');
+
+  tabsPanel.forEach(panel => {
+    panel.classList.remove('active');
+  });
+
+  const anchorRef = e.target;
+  const activePanelId = anchorRef.getAttribute('href');
+  const activePanel = document.querySelector(activePanelId);
+
+  activePanel.classList.add('active');
+};
+
+const onQuestionClick = e => {
+  e.preventDefault();
+
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  tabs[1].classList.add('active');
+
+  const tabsPanel = document.querySelectorAll('.wdgt-single-panel');
+  const tab2 = document.querySelector('#tab-2');
+
+  tabsPanel.forEach(panel => {
+    panel.classList.remove('active');
+  });
+
+  tab2.classList.add('active');
+
+  const tabQuestion = document.querySelectorAll('.wdgt-panel-content');
+
+  tabQuestion.forEach(question => {
+    question.classList.remove('active');
+  });
+
+  const questionAnchor = e.target;
+  const activeQuestion = questionAnchor.getAttribute('href');
+  const AnswerPanel = document.querySelector(activeQuestion);
+
+  AnswerPanel.classList.add('active');
+};
+
+for (const tab of tabs) {
+  tab.addEventListener('click', onTabClick);
+}
+
+for (const question of panelQuestions) {
+  question.addEventListener('click', onQuestionClick);
+}
+
+
+};
+
+export default linkJS;
+
+
