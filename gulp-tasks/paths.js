e9164d3f0ec08d129581b319d18c9ba17c4e3f52
Nathan Rogan <nathan.rogan@wmca.org.uk>
2021-04-14 17:14:20 +0100

    new seperate grid only css

--- gulp-tasks/paths.js
+++ gulp-tasks/paths.js
@@ -10,7 +10,8 @@ module.exports = {
     minifySrc: [
       'src/wmcads/assets/sass/wmcads.scss',
       'src/www/wmcads-website.scss',
-      'src/www/pages/templates/templates.scss'
+      'src/www/pages/templates/templates.scss',
+      'src/wmcads/assets/sass/wmcads-grid/grid.scss'
     ], // List of scss file(s) which should be processed, linted & minified
     output: 'build/css/', // output location of minified styles
     reactNativeSrc: 'src/wmcads/assets/sass/wmcads.scss'
