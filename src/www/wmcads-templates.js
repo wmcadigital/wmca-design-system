0687c44942b1555bb8d7580c39fb9f3c43482a6d
Nathan Rogan <rolorogan@gmail.com>
2021-07-05 16:56:15 +0100

    feat: search template (#199)
    * update breadcrumb to use text instead of an icon
    
    * new search template
    
    * update search js location
    
    Co-authored-by: Nathan Rogan <nathan.rogan@wmca.org.uk>

--- src/www/wmcads-templates.js
+++ src/www/wmcads-templates.js
@@ -1,3 +1,4 @@
 import pageContents from './pages/templates/campaign-two-columns/campaign-template';
+import search from './pages/templates/search/search-template';
 
-window.addEventListener('DOMContentLoaded', pageContents());
+window.addEventListener('DOMContentLoaded', pageContents(), search());
