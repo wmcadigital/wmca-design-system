85bcb6d68dd5d0077938136121dbcf69a35358f1
Nathan Rogan <nathan.rogan@wmca.org.uk>
2021-07-05 16:25:13 +0100

    new search template

--- src/www/wmcads-templates.js
+++ src/www/wmcads-templates.js
@@ -1,3 +1,4 @@
 import pageContents from './pages/templates/campaign-two-columns/campaign-template';
+import search from './pages/templates/campaign-two-columns/campaign-template';
 
-window.addEventListener('DOMContentLoaded', pageContents());
+window.addEventListener('DOMContentLoaded', pageContents(), search());
