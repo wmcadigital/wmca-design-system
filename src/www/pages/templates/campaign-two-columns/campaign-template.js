accd28d889b4b3fc992087e68edeaac9e0c3868e
Nathan Rogan <nathan.rogan@wmca.org.uk>
2021-07-07 14:02:53 +0100

    search page amends & fixes

--- src/www/pages/templates/campaign-two-columns/campaign-template.js
+++ src/www/pages/templates/campaign-two-columns/campaign-template.js
@@ -1,33 +1,37 @@
 const pageContents = () => {
-  const headings = document.querySelectorAll('main h2');
+  const contents = document.querySelector('.wmcads-page-contents');
 
-  headings.forEach(heading => {
-    const str = heading.innerHTML;
-    const linkStr = str.toLowerCase().replaceAll(' ', '-');
-    const link = `#${linkStr}`;
+  if (contents != null) {
+    const headings = document.querySelectorAll('main h2');
 
-    // add id to all the h2
-    heading.setAttribute('id', linkStr);
+    headings.forEach(heading => {
+      const str = heading.innerHTML;
+      const linkStr = str.toLowerCase().replaceAll(' ', '-');
+      const link = `#${linkStr}`;
 
-    // function to decode htmlentities
-    function decodeHtml(html) {
-      const txt = document.createElement('textarea');
-      txt.innerHTML = html;
-      return txt.value;
-    }
-    // function to add a link to the page contents
-    function createMenuItem() {
-      const li = document.createElement('a');
-      li.textContent = decodeHtml(str);
-      li.setAttribute('href', link);
-      return li;
-    }
+      // add id to all the h2
+      heading.setAttribute('id', linkStr);
 
-    // get the page contents
-    const menu = document.querySelector('.wmcads-page-contents');
-    // add page contents link
-    menu.appendChild(createMenuItem());
-  });
+      // function to decode htmlentities
+      function decodeHtml(html) {
+        const txt = document.createElement('textarea');
+        txt.innerHTML = html;
+        return txt.value;
+      }
+      // function to add a link to the page contents
+      function createMenuItem() {
+        const li = document.createElement('a');
+        li.textContent = decodeHtml(str);
+        li.setAttribute('href', link);
+        return li;
+      }
+
+      // get the page contents
+      const menu = document.querySelector('.wmcads-page-contents');
+      // add page contents link
+      menu.appendChild(createMenuItem());
+    });
+  }
 };
 
 export default pageContents;
